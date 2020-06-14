b96997020c31a97203d8427a693d0ce5148a9623
mentefria <66900535+mentefria@users.noreply.github.com>
2020-06-14 17:13:12 +0300

    Add files via upload

---
+++ 25/25/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _25
+{
+    class Program
+    {
+        static void CreateLine(string c)
+        {
+            Console.WriteLine("введите количество элементов");
+            int numberOfElements = int.Parse(Console.ReadLine());
+            string[] array = new string[numberOfElements];
+            string result = "";
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.WriteLine($"{i} элемент ");
+                array[i] = Console.ReadLine();
+                result += array[i];
+                if(i != numberOfElements - 1)
+                {
+                    result += c;
+                }
+            }
+            Console.WriteLine(result);
+            
+            
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите симбол, которым хотите рвзделить элементы");
+            string coma = Console.ReadLine();
+            CreateLine(coma);
+        }
+    }
+}
